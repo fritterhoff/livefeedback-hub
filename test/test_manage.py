530f0fb422d83705294b683732aa2e2ddce02b66
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-12 10:55:13 +0200

    More tests + using in memory db for tests

--- test/test_manage.py
+++ test/test_manage.py
@@ -1,11 +1,15 @@
+import os
 from unittest.mock import MagicMock, patch
 
 from otter.grade import utils
 from python_on_whales.exceptions import NoSuchImage
+from tornado.httputil import HTTPFile
 
 from livefeedback_hub.db import AutograderZip
 from livefeedback_hub.handlers import manage
 from livefeedback_hub.server import JupyterService
+
+os.environ["SERVICE_DB_URL"] = "sqlite://"
 
 
 class TestManage:
@@ -28,3 +32,23 @@ class TestManage:
         service.log.warning = MagicMock()
         manage.delete_docker_image(service, zip)
         service.log.warning.assert_called_once()
+
+    @patch("python_on_whales.docker.image.exists")
+    def test_build_update_non_existing(self, exists: MagicMock):
+        service = JupyterService()
+        zip = HTTPFile()
+        zip["body"] = bytes("Test", "utf-8")
+        exists.return_value = True
+        manage.build(service, "1", zip_file=zip, update=True)
+        exists.assert_not_called()
+
+    @patch("python_on_whales.docker.image.exists")
+    def test_build_update_same(self, exists: MagicMock):
+        service = JupyterService()
+        zip = HTTPFile()
+        zip["body"] = bytes("Test", "utf-8")
+        exists.return_value = True
+        with service.session() as session:
+            session.add(AutograderZip(id="1"))
+        manage.build(service, "1", zip_file=zip, update=True)
+        exists.assert_called_once_with(f"{utils.OTTER_DOCKER_IMAGE_TAG}:0cbc6611f5540bd0809a388dc95a615b")
