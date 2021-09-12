b55131e43424c6a351dcac05bb8f92b350c2338d
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-12 13:40:45 +0200

    Further tests

--- test/test_manage.py
+++ test/test_manage.py
@@ -1,5 +1,5 @@
 import os
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 import pytest
 from otter.grade import utils
@@ -14,7 +14,6 @@ os.environ["SERVICE_DB_URL"] = "sqlite://"
 
 
 class TestManage:
-
     @pytest.fixture()
     def service(self):
         service = JupyterService()
@@ -51,6 +50,33 @@ class TestManage:
         zip["body"] = bytes("Test", "utf-8")
         exists.return_value = True
         with service.session() as session:
-            session.add(AutograderZip(id="1"))
+            session.add(AutograderZip(id="1", ready=False))
         manage.build(service, "1", zip_file=zip, update=True)
         exists.assert_called_once_with(f"{utils.OTTER_DOCKER_IMAGE_TAG}:0cbc6611f5540bd0809a388dc95a615b")
+        with service.session() as session:
+            assert session.query(AutograderZip).filter_by(id="1").first().ready == True
+
+    @patch("python_on_whales.docker.image.exists")
+    @patch("python_on_whales.docker.build")
+    @patch("python_on_whales.docker.image.remove")
+    def test_build_update(self, delete: MagicMock, build: MagicMock, exists: MagicMock, service):
+        zip = HTTPFile()
+        zip["body"] = bytes("Test", "utf-8")
+        exists.return_value = False
+        with service.session() as session:
+            session.add(AutograderZip(id="1", ready=False, data=bytes("Old", "utf-8")))
+
+        manage.build(service, "1", zip_file=zip, update=True)
+
+        exists.assert_called_with(f"{utils.OTTER_DOCKER_IMAGE_TAG}:0cbc6611f5540bd0809a388dc95a615b")
+
+        delete.assert_called_once_with(f"{utils.OTTER_DOCKER_IMAGE_TAG}:c7268757fbabf48019f4984933539d8a", force=True)
+
+        build.assert_called_once()
+        args: call = build.call_args
+        assert args.kwargs["load"] == True
+        assert args.kwargs["tags"] == [f"{utils.OTTER_DOCKER_IMAGE_TAG}:0cbc6611f5540bd0809a388dc95a615b"]
+
+        with service.session() as session:
+            assert session.query(AutograderZip).filter_by(id="1").first().ready == True
+            assert session.query(AutograderZip).filter_by(id="1").first().data == bytes("Test", "utf-8")
