b429b06786bcd8eda4d50a1860dfc3c6fc336b3f
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-12 11:34:03 +0200

    Optimize manage workflow and fix tests

--- test/test_manage.py
+++ test/test_manage.py
@@ -1,11 +1,12 @@
 import os
 from unittest.mock import MagicMock, patch
 
+import pytest
 from otter.grade import utils
 from python_on_whales.exceptions import NoSuchImage
 from tornado.httputil import HTTPFile
 
-from livefeedback_hub.db import AutograderZip
+from livefeedback_hub.db import AutograderZip, Base
 from livefeedback_hub.handlers import manage
 from livefeedback_hub.server import JupyterService
 
@@ -14,9 +15,13 @@ os.environ["SERVICE_DB_URL"] = "sqlite://"
 
 class TestManage:
 
-    @patch("python_on_whales.docker.image.remove")
-    def test_delete_image(self, mock: MagicMock):
+    @pytest.fixture()
+    def service(self):
         service = JupyterService()
+        return service
+
+    @patch("python_on_whales.docker.image.remove")
+    def test_delete_image(self, mock: MagicMock, service):
         zip = AutograderZip()
         zip.data = bytes("Test", "utf-8")
         mock.return_value = None
@@ -24,8 +29,7 @@ class TestManage:
         mock.assert_called_once_with(f"{utils.OTTER_DOCKER_IMAGE_TAG}:0cbc6611f5540bd0809a388dc95a615b", force=True)
 
     @patch("python_on_whales.docker.image.remove")
-    def test_delete_image_fails(self, remove: MagicMock):
-        service = JupyterService()
+    def test_delete_image_fails(self, remove: MagicMock, service):
         zip = AutograderZip()
         zip.data = bytes("Test", "utf-8")
         remove.side_effect = NoSuchImage([], 0)
@@ -34,8 +38,7 @@ class TestManage:
         service.log.warning.assert_called_once()
 
     @patch("python_on_whales.docker.image.exists")
-    def test_build_update_non_existing(self, exists: MagicMock):
-        service = JupyterService()
+    def test_build_update_non_existing(self, exists: MagicMock, service):
         zip = HTTPFile()
         zip["body"] = bytes("Test", "utf-8")
         exists.return_value = True
@@ -43,8 +46,7 @@ class TestManage:
         exists.assert_not_called()
 
     @patch("python_on_whales.docker.image.exists")
-    def test_build_update_same(self, exists: MagicMock):
-        service = JupyterService()
+    def test_build_update_same(self, exists: MagicMock, service):
         zip = HTTPFile()
         zip["body"] = bytes("Test", "utf-8")
         exists.return_value = True
