aef9da93bd01628716af4b07d686e979ddd4b101
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-13 16:46:19 +0200

    Test process notebook

--- livefeedback_hub/handlers/submission.py
+++ livefeedback_hub/handlers/submission.py
@@ -12,15 +12,55 @@ import pandas as pd
 import stdio_proxy
 from jupyterhub.services.auth import HubOAuthenticated
 from otter.grade import containers, utils
-from livefeedback_hub.server import JupyterService
 from tornado.web import RequestHandler, authenticated
 
 from livefeedback_hub import core
-from livefeedback_hub.db import AutograderZip, Result, GUID_REGEX
+from livefeedback_hub.db import AutograderZip, GUID_REGEX, Result
+from livefeedback_hub.server import JupyterService
+
+executor = ThreadPoolExecutor(max_workers=16)
+
+
+def process_notebook(service: JupyterService, autograder_zip: bytes, notebook: bytes, id: str, user_hash: str):
+    tmp_dir = tempfile.mkdtemp()
+    fd, path = tempfile.mkstemp(suffix=".ipynb", dir=tmp_dir)
+    fd_zip, path_zip = tempfile.mkstemp(suffix=".zip", dir=tmp_dir)
+    cwd = os.getcwd()
+    try:
+        with os.fdopen(fd, "wb") as tmp:
+            tmp.write(notebook)
+            tmp.flush()
+        with os.fdopen(fd_zip, "wb") as tmp:
+            tmp.write(autograder_zip)
+            tmp.flush()
+
+        os.chdir(tmp_dir)
+        service.log.info(f"Launching otter-grader for {user_hash} and {id}")
+        image = utils.OTTER_DOCKER_IMAGE_TAG + ":" + containers.generate_hash(os.path.basename(path_zip))
+        stdout = BytesIO()
+        stderr = BytesIO()
+        with stdio_proxy.redirect_stdout(stdout), stdio_proxy.redirect_stderr(stderr):
+            user_result = containers.grade_assignments(path, image, debug=True, verbose=True)
+        add_or_update_results(service, user_hash, id, user_result)
+        service.log.info(f"Grading complete for {user_hash} and {id}")
+    except Exception as e:
+        service.log.exception(e)
+    finally:
+        os.chdir(cwd)
+        shutil.rmtree(tmp_dir)
+
+
+def add_or_update_results(service, user_hash, assignment_id, user_result: pd.DataFrame):
+    with service.session() as session:
+        existing: Optional[Result] = session.query(Result).filter_by(assignment=assignment_id, user=user_hash).first()
+        if existing:
+            existing.data = user_result.to_csv(index=False)
+        else:
+            result = Result(user=user_hash, assignment=assignment_id, data=user_result.to_csv(index=False))
+            session.add(result)
 
 
 class FeedbackSubmissionHandler(HubOAuthenticated, RequestHandler):
-    executor = ThreadPoolExecutor(max_workers=16)
 
     @staticmethod
     def _create_pattern() -> re.Pattern:
@@ -36,7 +76,8 @@ class FeedbackSubmissionHandler(HubOAuthenticated, RequestHandler):
     def _get_autograding_zip(self, nb) -> Tuple[Optional[str], Optional[bytes]]:
         cells = [cell["source"] for cell in nb["cells"]]
         pattern = self._create_pattern()
-        live_ids = [self._check_line(pattern, line) for item in cells for line in item.split("\n") if self._check_line(pattern, line)]
+        live_ids = [self._check_line(pattern, line) for item in cells for line in item.split("\n") if
+                    self._check_line(pattern, line)]
 
         if len(live_ids) == 0:
             self.log.info("No live feedback id in notebook")
@@ -68,47 +109,10 @@ class FeedbackSubmissionHandler(HubOAuthenticated, RequestHandler):
 
         user_hash = core.get_user_hash(self.get_current_user())
 
-        self.executor.submit(self.process_notebook, autograder_zip=autograder_zip, notebook=self.request.body, id=id, user_hash=user_hash)
+        executor.submit(process_notebook, service=self.service, autograder_zip=autograder_zip,
+                        notebook=self.request.body, id=id, user_hash=user_hash)
         await self.finish()
-
-    def process_notebook(self, autograder_zip: bytes, notebook: bytes, id: str, user_hash: str):
-        tmp_dir = tempfile.mkdtemp()
-        fd, path = tempfile.mkstemp(suffix=".ipynb", dir=tmp_dir)
-        fd_zip, path_zip = tempfile.mkstemp(suffix=".zip", dir=tmp_dir)
-        cwd = os.getcwd()
-        try:
-            with os.fdopen(fd, "wb") as tmp:
-                tmp.write(notebook)
-                tmp.flush()
-            with os.fdopen(fd_zip, "wb") as tmp:
-                tmp.write(autograder_zip)
-                tmp.flush()
-
-            os.chdir(tmp_dir)
-            self.log.info(f"Launching otter-grader for {user_hash} and {id}")
-            image = utils.OTTER_DOCKER_IMAGE_TAG + ":" + containers.generate_hash(os.path.basename(path_zip))
-
-            stdout = BytesIO()
-            stderr = BytesIO()
-            with stdio_proxy.redirect_stdout(stdout), stdio_proxy.redirect_stderr(stderr):
-                user_result = containers.grade_assignments(path, image, debug=True, verbose=True)
-            self.add_or_update_results(user_hash, id, user_result)
-            self.log.info(f"Grading complete for {user_hash} and {id}")
-        except Exception as e:
-            self.log.exception(e)
-        finally:
-            os.chdir(cwd)
-            shutil.rmtree(tmp_dir)
 
     def initialize(self, service: JupyterService):
         self.service = service
         self.log: logging.Logger = service.log
-
-    def add_or_update_results(self, user_hash, assignment_id, user_result: pd.DataFrame):
-        with self.service.session() as session:
-            existing: Optional[Result] = session.query(Result).filter_by(assignment=assignment_id, user=user_hash).first()
-            if existing:
-                existing.data = user_result.to_csv(index=False)
-            else:
-                result = Result(user=user_hash, assignment=assignment_id, data=user_result.to_csv(index=False))
-                session.add(result)
--- test/test_submission.py
+++ test/test_submission.py
@@ -2,16 +2,46 @@ import os
 import time
 from unittest.mock import MagicMock, patch
 
+import pandas as pd
 from tornado.testing import AsyncHTTPTestCase
 
 from livefeedback_hub import core
 from livefeedback_hub.db import AutograderZip, Result
+from livefeedback_hub.handlers import submission
 from livefeedback_hub.server import JupyterService
 
 os.environ["SERVICE_DB_URL"] = "sqlite:///:memory:"
 
 notebook = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "# LIVE: 333e2069-612e-4e0c-a4ac-e6ec1eaa44f0" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
 notebook_without_id = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
+
+
+class TestSubmission:
+
+    @patch("otter.grade.containers.grade_assignments")
+    def test_process_notebook(self, grade: MagicMock):
+        service = JupyterService()
+        grade.return_value = pd.DataFrame()
+        submission.process_notebook(service, bytes("", "utf-8"), bytes("", "utf-8"), "test", "test")
+        grade.assert_called_once()
+        with service.session() as session:
+            assert session.query(Result).first().user == "test"
+
+    @patch("otter.grade.containers.grade_assignments")
+    def test_process_notebook_twice(self, grade: MagicMock):
+        service = JupyterService()
+        grade.return_value = pd.DataFrame()
+        submission.process_notebook(service, bytes("", "utf-8"), bytes("test", "utf-8"), "test", "test")
+        submission.process_notebook(service, bytes("", "utf-8"), bytes("test-2", "utf-8"), "test", "test")
+        with service.session() as session:
+            assert session.query(Result).first().user == "test"
+            assert session.query(Result).count() == 1
+
+        submission.process_notebook(service, bytes("", "utf-8"), bytes("test-3", "utf-8"), "test", "test1")
+
+        with service.session() as session:
+            assert session.query(Result).first().user == "test"
+            assert session.query(Result).count() == 2
 
 
 class TestSubmissionHandler(AsyncHTTPTestCase):
@@ -53,11 +83,37 @@ class TestSubmissionHandler(AsyncHTTPTestCase):
 
     @patch("jupyterhub.services.auth.HubAuthenticated.get_current_user")
     @patch("otter.grade.containers.grade_assignments")
-    def test_submit(self, grade: MagicMock, get_current_user_mock: MagicMock):
+    @patch("livefeedback_hub.handlers.submission.add_or_update_results")
+    def test_submit_failing(self, add_or_update_results: MagicMock, grade: MagicMock, get_current_user_mock: MagicMock):
         get_current_user_mock.return_value = {"name": "student"}
+        grade.side_effect = Exception()
+        self.service.log.exception = MagicMock()
         with self.service.session() as session:
-            zip = AutograderZip(id="333e2069-612e-4e0c-a4ac-e6ec1eaa44f0", description="Test", ready=True, data=bytes("Old", "utf-8"), owner=core.get_user_hash(get_current_user_mock.return_value))
+            zip = AutograderZip(id="333e2069-612e-4e0c-a4ac-e6ec1eaa44f0", description="Test", ready=True,
+                                data=bytes("Old", "utf-8"),
+                                owner=core.get_user_hash(get_current_user_mock.return_value))
             session.add(zip)
+        response = self.fetch("/submit", method="POST", body=notebook)
+        assert response.code == 200
+        time.sleep(2)
+        grade.assert_called_once()
+        args = grade.call_args
+        assert args.args[1] == "otter-grade:c7268757fbabf48019f4984933539d8a"
+        self.service.log.exception.assert_called_once()
+        add_or_update_results.assert_not_called()
+
+    @patch("jupyterhub.services.auth.HubAuthenticated.get_current_user")
+    @patch("otter.grade.containers.grade_assignments")
+    def test_submit_success(self, grade: MagicMock, get_current_user_mock: MagicMock):
+        get_current_user_mock.return_value = {"name": "student"}
+        grade.return_value = pd.DataFrame()
+        self.service.log.exception = MagicMock()
+        with self.service.session() as session:
+            zip = AutograderZip(id="333e2069-612e-4e0c-a4ac-e6ec1eaa44f0", description="Test", ready=True,
+                                data=bytes("Old", "utf-8"),
+                                owner=core.get_user_hash(get_current_user_mock.return_value))
+            session.add(zip)
+
         response = self.fetch("/submit", method="POST", body=notebook)
         assert response.code == 200
         time.sleep(2)
