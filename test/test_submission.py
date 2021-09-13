8764084886bff3ea17e2c140c559908b8fc48f73
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-13 13:36:01 +0200

    Style fix in test

--- test/test_submission.py
+++ test/test_submission.py
@@ -5,8 +5,8 @@ from tornado.testing import AsyncHTTPTestCase
 from livefeedback_hub.db import AutograderZip, Result
 from livefeedback_hub.server import JupyterService
 
-
 notebook = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "# LIVE: 333e2069-612e-4e0c-a4ac-e6ec1eaa44f0" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
+
 
 class TestSubmissionHandler(AsyncHTTPTestCase):
     service = JupyterService(xsrf_cookies=False)
