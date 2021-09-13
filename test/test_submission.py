4e0db6399279ad871bb943e50f79961673db2f5f
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-13 13:44:39 +0200

    Simulate working submission

--- test/test_submission.py
+++ test/test_submission.py
@@ -1,9 +1,14 @@
+import os
+import time
 from unittest.mock import MagicMock, patch
 
 from tornado.testing import AsyncHTTPTestCase
 
+from livefeedback_hub import core
 from livefeedback_hub.db import AutograderZip, Result
 from livefeedback_hub.server import JupyterService
+
+os.environ["SERVICE_DB_URL"] = "sqlite:///:memory:"
 
 notebook = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "# LIVE: 333e2069-612e-4e0c-a4ac-e6ec1eaa44f0" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
 
@@ -38,3 +43,15 @@ class TestSubmissionHandler(AsyncHTTPTestCase):
         get_current_user_mock.return_value = {"name": "student"}
         response = self.fetch("/submit", method="POST", body=notebook)
         assert response.code == 200
+
+    @patch("jupyterhub.services.auth.HubAuthenticated.get_current_user")
+    @patch("otter.grade.containers.grade_assignments")
+    def test_submit(self, grade: MagicMock, get_current_user_mock: MagicMock):
+        get_current_user_mock.return_value = {"name": "student"}
+        with self.service.session() as session:
+            zip = AutograderZip(id="333e2069-612e-4e0c-a4ac-e6ec1eaa44f0", description="Test", ready=True, data=bytes("Old", "utf-8"), owner=core.get_user_hash(get_current_user_mock.return_value))
+            session.add(zip)
+        response = self.fetch("/submit", method="POST", body=notebook)
+        assert response.code == 200
+        time.sleep(2)
+        grade.assert_called_once()
