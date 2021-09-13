4c69243bd4bf4f58545dadbfe05ef98be278d446
Florian Ritterhoff <florian@ritterhoff.org>
2021-09-13 13:51:27 +0200

    Test notebooks without ID

--- test/test_submission.py
+++ test/test_submission.py
@@ -11,6 +11,7 @@ from livefeedback_hub.server import JupyterService
 os.environ["SERVICE_DB_URL"] = "sqlite:///:memory:"
 
 notebook = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "# LIVE: 333e2069-612e-4e0c-a4ac-e6ec1eaa44f0" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
+notebook_without_id = '{ "cells": [ { "cell_type": "code", "metadata": {}, "source": "" } ], "metadata": { "kernelspec": { "display_name": "Python 3", "language": "python", "name": "python3" }, "language_info": { "codemirror_mode": { "name": "ipython", "version": 3 }, "file_extension": ".py", "mimetype": "text/x-python", "name": "python", "nbconvert_exporter": "python", "pygments_lexer": "ipython3", "version": "3.6.5" }, "varInspector": { "cols": { "lenName": 16, "lenType": 16, "lenVar": 40 }, "kernels_config": { "python": { "delete_cmd_postfix": "", "delete_cmd_prefix": "del ", "library": "var_list.py", "varRefreshCmd": "print(var_dic_list())" }, "r": { "delete_cmd_postfix": ") ", "delete_cmd_prefix": "rm(", "library": "var_list.r", "varRefreshCmd": "cat(var_dic_list()) " } }, "types_to_exclude": [ "module", "function", "builtin_function_or_method", "instance", "_Feature" ], "window_display": false } }, "nbformat": 4, "nbformat_minor": 4}'
 
 
 class TestSubmissionHandler(AsyncHTTPTestCase):
@@ -42,6 +43,12 @@ class TestSubmissionHandler(AsyncHTTPTestCase):
     def test_submit_not_found(self, get_current_user_mock: MagicMock):
         get_current_user_mock.return_value = {"name": "student"}
         response = self.fetch("/submit", method="POST", body=notebook)
+        assert response.code == 200
+
+    @patch("jupyterhub.services.auth.HubAuthenticated.get_current_user")
+    def test_submitt_no_id(self, get_current_user_mock: MagicMock):
+        get_current_user_mock.return_value = {"name": "student"}
+        response = self.fetch("/submit", method="POST", body=notebook_without_id)
         assert response.code == 200
 
     @patch("jupyterhub.services.auth.HubAuthenticated.get_current_user")
